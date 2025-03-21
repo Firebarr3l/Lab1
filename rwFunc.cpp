96495e3d3e4c6c5468f6d47481fdd4c6a8932137
F1rebarr3l <Daniilklishin17@mail.ru>
2025-03-21 20:52:38 +0300

    Дабовление структуры и изменения функций под нее

--- rwFunc.cpp
+++ rwFunc.cpp
@@ -1,9 +1,10 @@
 #include "rwFunc.hpp"
 #include <fstream>
 #include <iostream>
+#include <sstream>
 
 // Функция для чтения данных из файла
-void readFromFile(const std::string& filename, std::vector<std::string>& data) {
+void readFromFile(const std::string& filename, std::vector<GardenRecord>& data) {
     std::ifstream file(filename);
     if (!file.is_open()) {
         std::cerr << "Ошибка при открытии файла для чтения!" << std::endl;
@@ -12,22 +13,44 @@ void readFromFile(const std::string& filename, std::vector<std::string>& data) {
 
     std::string line;
     while (std::getline(file, line)) {
-        data.push_back(line); // Добавляем строку в вектор
+        std::stringstream ss(line);
+        GardenRecord garden;
+        std::string temp;
+        // Считывание данных для каждого поля
+        std::getline(ss, temp, ';');
+        garden.id = std::stoi(temp); // Преобразуем в целое число
+
+        std::getline(ss, temp, ';');
+        garden.number = std::stoi(temp); // Преобразуем в целое число
+
+        std::getline(ss, garden.name, ';'); // Имя и фамилия
+
+        std::getline(ss, garden.phone, ';'); // Телефон
+
+        std::getline(ss, garden.address, ';'); // Адрес
+
+        data.push_back(garden); // Добавляем структуру в вектор
     }
+
 
     file.close();
 }
 
 // Функция для записи данных в файл
-void writeToFile(const std::string& filename, const std::vector<std::string>& data) {
+void writeToFile(const std::string& filename, const std::vector<GardenRecord>& data) {
     std::ofstream file(filename);
     if (!file.is_open()) {
         std::cerr << "Ошибка при открытии файла для записи!" << std::endl;
         return;  // Выход из функции при ошибке открытия файла
     }
 
-    for (const auto& line : data) {
-        file << line << std::endl; // Записываем каждую строку в файл
+    for (const auto& garden : data) {
+        file << garden.id << ";"
+             << garden.number << ";"
+             << garden.name << ";"
+             << garden.phone << ";"
+             << garden.address << ";"
+             << std::endl;  // Записываем данные в файл
     }
 
     file.close();
--- rwFunc.hpp
+++ rwFunc.hpp
@@ -4,6 +4,15 @@
 #include <string>
 #include <vector>
 
+
+struct GardenRecord {
+    int id;             // порядковый номер записи
+    int number;            // номер участка
+    std::string name;   // имя и фамилия
+    std::string phone;  // телефон
+    std::string address; // адрес
+};
+
 // Прототип функции для чтения данных из файла
 void readFromFile(const std::string& filename, std::vector<std::string>& data);
 
